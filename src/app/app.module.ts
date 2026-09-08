d091821a7d290ac162d063dd64e9690d4e01f08e
Harshit Panchal <harshitpanchal9428@gmail.com>
2026-09-08 18:28:35 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,78 +1,98 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {
-  HTTP_INTERCEPTORS,
-  HttpClientModule,
-} from '@angular/common/http';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpServiceService } from './http-service.service';
+import { EndpointServiceService } from './endpoint-service.service';
+import { ServiceLocatorService } from './service-locator.service';
+import { AuthServiceService } from './auth-service.service';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './login/signup.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user/user-list.component';
 import { RoleComponent } from './role/role.component';
 import { StudentComponent } from './student/student.component';
 import { FacultyComponent } from './faculty/faculty.component';
-import { CollegeComponent } from './college/college.component';
-import { UserComponent } from './user/user.component';
-import { MarksheetComponent } from './marksheet/marksheet.component';
-import { SubjectComponent } from './subject/subject.component';
 import { CourseComponent } from './course/course.component';
+import { SubjectComponent } from './subject/subject.component';
+import { MarksheetComponent } from './marksheet/marksheet.component';
+import { CollegeComponent } from './college/college.component';
 import { TimetableComponent } from './timetable/timetable.component';
-import { FormsModule } from '@angular/forms';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { RoleListComponent } from './role/role-list.component';
-import { UserListComponent } from './user/user-list.component';
-import { MarksheetListComponent } from './marksheet/marksheet-list.component';
+import { FooterComponent } from './footer/footer.component';
 import { CollegeListComponent } from './college/college-list.component';
-import { CourseListComponent } from './course/course-list.component';
 import { StudentListComponent } from './student/student-list.component';
+import { MarksheetListComponent } from './marksheet/marksheet-list.component';
+import { CourseListComponent } from './course/course-list.component';
 import { SubjectListComponent } from './subject/subject-list.component';
-import { FacultyListComponent } from './faculty/faculty-list.component';
 import { TimetableListComponent } from './timetable/timetable-list.component';
-import { AuthServiceService } from './auth-service.service';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './login/signup.component';
-import { ForgotpasswordComponent } from './login/forgotpassword.component';
+import { FacultyListComponent } from './faculty/faculty-list.component';
 import { MyprofileComponent } from './user/myprofile.component';
 import { ChangepasswordComponent } from './user/changepassword.component';
+import { ForgotpasswordComponent } from './login/forgotpassword.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    SignupComponent,
     NavbarComponent,
-    FooterComponent,
+    DashboardComponent,
+    UserComponent,
+    UserListComponent,
     RoleComponent,
     StudentComponent,
     FacultyComponent,
-    CollegeComponent,
-    UserComponent,
-    MarksheetComponent,
-    SubjectComponent,
     CourseComponent,
+    SubjectComponent,
+    MarksheetComponent,
+    CollegeComponent,
     TimetableComponent,
-    DashboardComponent,
     RoleListComponent,
-    UserListComponent,
-    MarksheetListComponent,
+    FooterComponent,
     CollegeListComponent,
-    CourseListComponent,
     StudentListComponent,
+    MarksheetListComponent,
+    CourseListComponent,
     SubjectListComponent,
-    FacultyListComponent,
     TimetableListComponent,
-    LoginComponent,
-    SignupComponent,
-    ForgotpasswordComponent,
+    FacultyListComponent,
     MyprofileComponent,
     ChangepasswordComponent,
+    ForgotpasswordComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    FormsModule
+  ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthServiceService,
-      multi: true,
-    }
+    HttpServiceService,
+    EndpointServiceService,
+    ServiceLocatorService,
+    AuthServiceService
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpServiceService } from '../http-service.service';
 import { ServiceLocatorService } from '../service-locator.service';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-navbar',
@@ -15,8 +17,15 @@ export class NavbarComponent {
     data: {}
   }
 
-  constructor(private httpService: HttpServiceService, private router: Router, private servicelocator: ServiceLocatorService) {
+  constructor(private translate: TranslateService ,private httpService: HttpServiceService, private router: Router, private servicelocator: ServiceLocatorService) {
+      const locale = localStorage.getItem("locale") || 'en';
+      translate.setDefaultLang(locale);
+      translate.use(locale);
+  }
 
+   changeLocale(locale: string) {
+    localStorage.setItem("locale", locale);
+    this.translate.use(locale);
   }
 
   isLogin() {
